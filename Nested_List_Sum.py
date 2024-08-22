a200b5df41feab76cf0d552f6ef8ea707c06d2d7
Alok <144205496+Alokiitdh@users.noreply.github.com>
2024-08-22 21:43:16 +0530

    Update Nested_List_Sum.py

--- Nested_List_Sum.py
+++ Nested_List_Sum.py
@@ -1,10 +1,15 @@
 # List
 num = []
-sum = 0
-numbers =[[1,2],[3],[4,5,6]]
-for i in range(len(numbers)):
-    num = numbers[i]
-    for k in range(len(num)):
-        sum = sum + num[k]
-print(sum)
 
+my_list =[[1,2],[3],[4,5,6]]
+
+def nested_sum(my_list):
+    sum = 0
+    for i in range(len(my_list)):
+        num = my_list[i]
+        for k in range(len(num)):
+            sum = sum + num[k]
+    return(sum)
+
+sum_List = nested_sum(my_list)
+print(sum_List)
