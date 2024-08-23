c47c19b907f985503c47581309e345ab1c2c412b
Alok <144205496+Alokiitdh@users.noreply.github.com>
2024-08-23 10:10:06 +0530

    intro_to_dict

---
+++ Dictonaries_introductions.py
@@ -0,0 +1,8 @@
+#Dictonaries
+my_dict = dict()
+# Key Value Pair: where key is ONE and Value is UNO
+my_dict['One'] = 'UNO'
+my_dict['Two'] = 'Pun'
+my_dict['Three'] = 'Duno'
+print(my_dict)
+print(my_dict.values()) # it is returning the va,ues towarda th key values
